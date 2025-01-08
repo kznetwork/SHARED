e4916432844ce705097891b59a8ec9a8288a4ab6
kznetwork <151503815+kznetwork@users.noreply.github.com>
2025-01-09 07:41:30 +0900

    20250109 HelloJava
    20250109 HelloJava 추가압니다.

---
+++ HelloJava/src/main/java/com/samsung/HelloJava.java
@@ -0,0 +1,14 @@
+package com.samsung;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println(getHelloJava());
+    }
+
+    protected static String getHelloJava() {
+        return "Hello, Java!";
+    }
+
+
+}
